770a7a1ee34acbac7877140ee9501df396381061
Mr.Eagle <mreagle.design@gmail.com>
2019-11-25 12:51:19 -0500

    Added About -needs Fix responsive

--- app_server/controllers/aboutController.js
+++ app_server/controllers/aboutController.js
@@ -3,11 +3,10 @@ module.exports.index = (req, res) => {
       title: 'About',
       mainTitle: 'ABOUT',
       history1: 'El Águila es un ave de alto vuelo.',
-      history2: 'Representa la fuerza',
-      history3: 'El ojo de águila puede ver más allá',
-      history4: 'Eagle Design está basada',
-      history5: 'Volando alto',
-      history6: 'Llegando lejos',
+      history2: 'Representa la fuerza <br/> perseverancia y tenacidad.',
+      history3: 'El ojo de águila puede ver más allá <br/> de lo que cualquier otra ave.',
+      history4: 'Eagle Design está basada <br/> en estos principios, <br/> de modo que ve más allá <br/> de lo que todos pueden.',
+      history5: 'Volando alto <br/> Llegando lejos',
       projectsTitle: 'Últimos Proyectos'
    });
 };
